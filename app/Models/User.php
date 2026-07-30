59cb237b73e7325cb2237ccd9c1e95216cd11e4c
Nguyễn Hoàng Phúc <phucnh.flamedia@gmail.com>
2026-07-30 17:45:37 +0700

    feat(webhook): opt User into webhook events

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,14 @@
 
 namespace App\Models;
 
+use Lumina\Webhook\Traits\HasWebhookEvents;
+
 class User extends \Lumina\Customer\Models\User
 {
-    //
+    use HasWebhookEvents;
+
+    protected static function webhookEvents(): array
+    {
+        return ['created', 'updated'];
+    }
 }
